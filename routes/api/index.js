52a5203dbd564ac4d6f9fe412ca3a9291198a664
Snowsore <snow_sore@hotmail.com>
2021-06-23 16:02:50 +0800

    Add goods

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,6 +5,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 router.use('/auth', require('./auth'))
 router.use('/users', require('./users'))
+router.use('/goods', require('./goods'))
 
 // router.use('/user', createProxyMiddleware({
 //     target: 'http://localhost:5000',
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -4,21 +4,20 @@ const express = require('express');
 const router = express.Router();
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const cors = require('cors')
-router.use(cors({
-    origin: 'http://test.com',
-    optionsSuccessStatus: 200
+router.use('*', createProxyMiddleware({
+    target: 'http://192.168.101.162',
+    changeOrigin: true,
 }))
-
-// router.use('*', createProxyMiddleware({
-//     target: 'http://192.168.101.162',
-//     changeOrigin: true,
-//     pathRewrite: {'^/user' : ''}
-// }))
 
 router.get('/', (req, res) => {
     res.json({
         msg: 'Good'
+    })
+})
+
+router.get('/test', (req, res) => {
+    res.json({
+        msg: 'testGood'
     })
 })
 
@@ -48,12 +47,42 @@ router.get('/group', (req, res) => {
     ])
 })
 
-router.get('/personal/:id', (req, res) => {
+router.get('/user/:id', (req, res) => {
     res.json([
         {
             name: 'Yajyu',
             age: 24,
             job: 'Student',
+        }
+    ])
+})
+
+router.get('/filter', (req, res) => {
+    console.log(req.params)
+    res.json([
+        {
+            "gender": "男",
+            "nick_name": "",
+            "index": 14,
+            "id": "111",
+            "email": "cxok1bzs6@gmail.com",
+            "username": "空祥"
+        },
+        {
+            "gender": "女",
+            "nick_name": "",
+            "index": 15,
+            "id": "112",
+            "email": "yceplcid@live.com",
+            "username": "戈怡馥"
+        },
+        {
+            "gender": "男",
+            "nick_name": "",
+            "index": 16,
+            "id": "113",
+            "email": "wqrwroqqf@live.com",
+            "username": "谈岩"
         }
     ])
 })
