bbae279dba2cf1e6fb861329b1d3b6d60ac3fdd7
Snowsore <snow_sore@hotmail.com>
2021-06-26 08:11:45 +0800

    dotenv

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -17,8 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-require('dotenv').config()
 
 app.use('/', indexRouter);
 
